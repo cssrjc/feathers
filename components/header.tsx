e11bb82d5da19dc40edfe5604c9c95c2259e4c07
mitul <mitu04112007@gmail.com>
2025-05-22 17:03:32 +0800

    random

--- components/header.tsx
+++ components/header.tsx
@@ -5,8 +5,8 @@ type HeaderProps = {
 
 export const Header = ({ header, desc }: HeaderProps) => {
   return (
-    <div className="w-full flex flex-col items-center sm:gap-3 mt-8 sm:mt-20">
-      <h1 className="font-sketch font-medium tracking-tight text-green-2 text-pri">{header}</h1>
+    <div className="w-full flex flex-col items-center gap-3 mt-10 sm:mt-20">
+      <h1 className="font-sketch font-medium tracking-tight text-green-2 text-6xl sm:text-pri">{header}</h1>
       <p className="font-sans text-green-2/40 text-ter">{desc}</p>
     </div>
   );
